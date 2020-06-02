ab56a239485a798ee9560b787464471576585ae3
Alexis Cárdenas <alexiscardednas85@hotmail.com>
2020-06-01 19:18:31 -0500

    Se definieron las capas que conformaran el proyecto(Capa de presentación, dominio, Datos, Servicios) y se relacionaron las capas. Close #1

---
+++ EcommercePresentacion/Ecommerce.Dominio/ProductoDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Dominio
+{
+    public class ProductoDTO
+    {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public Nullable<decimal> Precio { get; set; }
+        public short Stock { get; set; }
+        public string Descripcion { get; set; }
+        public string Referecnia { get; set; }
+    }
+}
